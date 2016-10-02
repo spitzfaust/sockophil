55dfaaa93a9696e189fb3b02dfbd328b71a11d46
Tobias Watzek <spitzfaust@gmail.com>
2016-10-02 11:20:41 +0200

    Added some methods to menu class.
    added some tests.

--- include/sockclient/menu.h
+++ include/sockclient/menu.h
@@ -13,8 +13,12 @@ namespace sockclient {
         std::string listening_on;
         std::string current_directory;
         bool first_run;
+        void render_welcome_msg() const noexcept;
+        void render_main_menu() const noexcept;
     public:
         Menu(int port, std::string ip_address, std::string current_directory);
         ClientSelection get_option();
+        std::string get_listening_on() const noexcept;
+        std::string get_current_directory() const noexcept;
     };
 }
--- src/sockclient/menu.cpp
+++ src/sockclient/menu.cpp
@@ -8,4 +8,12 @@ namespace sockclient {
     Menu::Menu(int port, std::string ip_address, std::string current_directory) : current_directory(current_directory), first_run(true) {
         this->listening_on = ip_address + ":" + std::to_string(port);
     }
+
+    std::string Menu::get_current_directory() const noexcept {
+        return this->current_directory;
+    }
+
+    std::string Menu::get_listening_on() const noexcept {
+        return this->listening_on;
+    }
 }
